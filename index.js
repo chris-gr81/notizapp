7dd5283629c88f696d9f18955952fe79e9faa7a9
Christian Grimm <grimmchristian@gmx.de>
2025-03-08 14:19:45 +0100

    Add basic data-structure and functions for rendering and sorting

---
+++ index.js
@@ -0,0 +1,71 @@
+const noteList = [
+  {
+    id: 1,
+    title: "Summertime",
+    text: "Das ist, wenn die Sonne Scheint.Glaub mir, so ist das.",
+    lastUpdated: 1741438060033,
+  },
+  {
+    id: 2,
+    title: "Spring",
+    text: "The spring is my love, There is a spring rezzling in the trees.",
+    lastUpdated: 1741438034150,
+  },
+  {
+    id: 3,
+    title: "Winter",
+    text: "Alles kalt, alles schneit. Furchtbar",
+    lastUpdated: 1741438274150,
+  },
+];
+
+console.log(noteList[0]);
+
+function createSidebarNote(noteItem) {
+  const sidebarNoteEl = document.createElement("div");
+  sidebarNoteEl.classList.add("note-preview");
+
+  const titleEl = document.createElement("div");
+  titleEl.classList.add("note-preview-header");
+  titleEl.appendChild(document.createTextNode(noteItem.title));
+
+  const textEl = document.createElement("div");
+  textEl.classList.add("note-preview-text");
+  textEl.appendChild(document.createTextNode(noteItem.text));
+
+  const timeEl = document.createElement("div");
+  timeEl.classList.add("note-preview-timestamp");
+  timeEl.appendChild(
+    document.createTextNode(formateTimestamp(noteItem.lastUpdated))
+  );
+  timeEl.create;
+
+  sidebarNoteEl.append(titleEl, textEl, timeEl);
+  sidebarNoteEl.setAttribute("id", noteItem.id);
+
+  return sidebarNoteEl;
+}
+
+function formateTimestamp(lastUpdated) {
+  return new Date(lastUpdated).toLocaleString("de-DE", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+}
+
+function renderSidebar() {
+  const sidebarList = document.getElementById("sidebar-list");
+  const sortedNoteList = noteList.toSorted(
+    (a, b) => Number(b.lastUpdated) - Number(a.lastUpdated)
+  );
+
+  sortedNoteList.forEach((e) => {
+    sidebarList.appendChild(createSidebarNote(e));
+  });
+}
+
+renderSidebar();
