d7f5abbbe0dd577f270300f2d20b86d892641013
Christian Grimm <grimmchristian@gmx.de>
2025-03-09 08:37:45 +0100

    Add notesAPI.js

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
 const sidebarListEl = document.querySelector(".sidebar-list");
-const noteList = [
-  {
-    id: 1,
-    title: "Summertime",
-    text: "Das ist, wenn die Sonne Scheint. Bacardi in der Hand und ab dafür.",
-    lastUpdated: 1741438060033,
-  },
-  {
-    id: 2,
-    title: "Spring",
-    text: "The spring is my love, There is a spring rezzling in the trees.",
-    lastUpdated: 1741438034150,
-  },
-  {
-    id: 3,
-    title: "Winter",
-    text: "Alles kalt, alles schneit. Furchtbar",
-    lastUpdated: 1741438274150,
-  },
-];
 
 function createSidebarNote(noteItem) {
   const sidebarNoteEl = document.createElement("div");
---
+++ notesAPI.js
@@ -0,0 +1,20 @@
+const noteList = [
+  {
+    id: 1,
+    title: "Summertime",
+    text: "Das ist, wenn die Sonne Scheint. Bacardi in der Hand und ab dafür.",
+    lastUpdated: 1741438060033,
+  },
+  {
+    id: 2,
+    title: "Spring",
+    text: "The spring is my love, There is a spring rezzling in the trees.",
+    lastUpdated: 1741438034150,
+  },
+  {
+    id: 3,
+    title: "Winter",
+    text: "Alles kalt, alles schneit. Furchtbar",
+    lastUpdated: 1741438274150,
+  },
+];
